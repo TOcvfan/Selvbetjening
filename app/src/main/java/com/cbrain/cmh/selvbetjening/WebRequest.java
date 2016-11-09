e1a2936569e666392fec9cfb51748ce2dc3a7043
TOcvfan <christian@hammervig.dk>
2016-11-09 09:09:37 +0100

    first working build

--- app/src/main/java/com/cbrain/cmh/selvbetjening/WebRequest.java
+++ app/src/main/java/com/cbrain/cmh/selvbetjening/WebRequest.java
@@ -1,10 +1,6 @@
 package com.cbrain.cmh.selvbetjening;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -14,27 +10,32 @@ import java.util.Map;
 import javax.net.ssl.HttpsURLConnection;
 
 public class WebRequest {
-    static String response = null;
-    public final static int GETRequest = 1;
-    public final static int POSTRequest = 2;
 
+    static String response = null;
+    public final static int GET = 1;
+    public final static int POST = 2;
+
+    //Constructor with no parameter
     public WebRequest() {
+
     }
+
     /**
      * Making web service call
      *
-     * @url – url to make web request
-     * @requestmethod – http request method
+     * @url - url to make request
+     * @requestmethod - http request method
      */
     public String makeWebServiceCall(String url, int requestmethod) {
         return this.makeWebServiceCall(url, requestmethod, null);
     }
+
     /**
-     * Making web service call
+     * Making service call
      *
-     * @url – url to make web request
-     * @requestmethod – http request method
-     * @params – http request params
+     * @url - url to make request
+     * @requestmethod - http request method
+     * @params - http request params
      */
     public String makeWebServiceCall(String urladdress, int requestmethod,
                                      HashMap<String, String> params) {
@@ -42,41 +43,46 @@ public class WebRequest {
         String response = "";
         try {
             url = new URL(urladdress);
+
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(15001);
-            conn.setConnectTimeout(15001);
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
             conn.setDoInput(true);
-            conn.setDoOutput(true);
-            if (requestmethod == POSTRequest) {
+
+            if (requestmethod == POST) {
                 conn.setRequestMethod("POST");
-            } else if (requestmethod == GETRequest) {
-                conn.setRequestMethod("GETRequest");
+            } else if (requestmethod == GET) {
+                conn.setRequestMethod("GET");
             }
 
             if (params != null) {
-                OutputStream ostream = conn.getOutputStream();
+                OutputStream os = conn.getOutputStream();
                 BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(ostream, "UTF-8"));
-                StringBuilder requestresult = new StringBuilder();
+                        new OutputStreamWriter(os, "UTF-8"));
+
+                StringBuilder result = new StringBuilder();
                 boolean first = true;
                 for (Map.Entry<String, String> entry : params.entrySet()) {
                     if (first)
                         first = false;
                     else
-                        requestresult.append("&");
-                    requestresult.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-                    requestresult.append("=");
-                    requestresult.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+                        result.append("&");
+
+                    result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                    result.append("=");
+                    result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
                 }
-                writer.write(requestresult.toString());
+
+                writer.write(result.toString());
 
                 writer.flush();
                 writer.close();
-                ostream.close();
+                os.close();
             }
-            int reqresponseCode = conn.getResponseCode();
 
-            if (reqresponseCode == HttpsURLConnection.HTTP_OK) {
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
                 String line;
                 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                 while ((line = br.readLine()) != null) {
@@ -88,6 +94,8 @@ public class WebRequest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return response;
     }
+
 }
